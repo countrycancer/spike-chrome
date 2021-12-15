8ad55196e033bfe905b5e7a449e46211ce3644f9
Your Name <you@example.com>
2021-12-15 06:23:20 +0000

    Show the number of h3 tags

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,14 @@ module Main where
 
 import Prelude
 
+import Data.Array (length)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (logShow)
+import Web.DOM.Document (getElementsByTagName)
+import Web.DOM.HTMLCollection (toArray)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toDocument)
+import Web.HTML.Window (document)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = window >>= document >>= toDocument >>> getElementsByTagName "h3" >>= toArray >>= logShow <<< length
