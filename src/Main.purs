1b30a7aab94f88a77189fc8336b218970038ebe6
Your Name <you@example.com>
2021-12-17 21:28:31 +0000

    Use getAttribute

--- src/Main.purs
+++ src/Main.purs
@@ -4,16 +4,16 @@ import Prelude
 
 import Data.Lens (itraverseOf_)
 import Data.Lens.Indexed (itraversed)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Effect (Effect)
 import Effect.Console (logShow)
 import Effect.Uncurried (EffectFn3, runEffectFn3)
 import Web.DOM (Document, Element)
 import Web.DOM.Document (createElement, createTextNode, getElementsByTagName)
-import Web.DOM.Element (setAttribute)
+import Web.DOM.Element (fromNode, getAttribute, setAttribute)
 import Web.DOM.Element as Element
 import Web.DOM.HTMLCollection (toArray)
-import Web.DOM.Node (appendChild)
+import Web.DOM.Node (appendChild, parentNode)
 import Web.DOM.Text as Text
 import Web.Event.Event (Event, EventType(..))
 import Web.Event.EventTarget (addEventListener, eventListener)
@@ -30,10 +30,10 @@ insertAdjacentElement = runEffectFn3 insertAdjacentElementImpl
 effectDocument :: Effect Document
 effectDocument = toDocument <$> (window >>= document)
 
-handle :: Int -> Event -> Effect Unit
-handle i e = case fromEvent e of
+handle :: Int -> String -> Event -> Effect Unit
+handle i href e = case fromEvent e of
     Nothing -> pure unit
-    Just keyboardEvent -> if show i == key keyboardEvent then logShow i else pure unit
+    Just keyboardEvent -> if show i == key keyboardEvent then logShow href else pure unit
 
 insertDiv :: Int -> Element -> Effect Unit
 insertDiv i h3 = do
@@ -43,7 +43,11 @@ insertDiv i h3 = do
     _ <- appendChild (Text.toNode text) (Element.toNode div)
     _ <- setAttribute "style" "float:left;left:-5px;position:absolute;" div
     insertAdjacentElement h3 "beforebegin" div
-    listener <- eventListener $ handle $ i + 1
+    maybeA <- parentNode (Element.toNode div)
+    href <- case maybeA >>= fromNode of
+        Nothing -> pure ""
+        Just element -> fromMaybe "" <$> getAttribute "href" element
+    listener <- eventListener $ handle (i + 1) href
     target <- toEventTarget <$> window
     addEventListener (EventType "keydown") listener false target
 
